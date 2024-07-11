3e9fd9f8a657a88c116ae1f2973f035c112703c2
sxt <2735950909@qq.com>
2024-07-12 06:50:53 +0800

    [feat] 优化import

--- src/main/java/ink/candle/candleReg/CandleReg.java
+++ src/main/java/ink/candle/candleReg/CandleReg.java
@@ -2,11 +2,9 @@ package ink.candle.candleReg;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
